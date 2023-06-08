7e6121a16ca1648b7656036dd870089344c934cd
madosuki <yurikasamaaikatsu@gmail.com>
2023-06-08 23:12:02 +0900

    add codes and archive_errno function

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,30 @@
- use libc::{c_int, c_char, c_void, size_t, off_t};
+use libc::{c_int, c_char, c_void, size_t, off_t};
 
-pub enum ArchiveError {
-    ArchiveEof = 1,
-    ArchiveOk = 0,
-    ArchiveRetry = -10,
-    ArchiveWarn = -20,
-    ArchiveFailed = -25,
-    ArchiveFatal = -30,
-}
+// Error codes
+pub const ARCHIVE_EOF: c_int = 1;
+pub const ARCHIVE_OK: c_int = 0;
+pub const ARCHIVE_RETRY: c_int = -10;
+pub const ARCHIVE_WARN: c_int = -20;
+pub const ARCHIVE_FAILED: c_int = -25;
+pub const ARCHIVE_FATAL: c_int = -30;
+
+// Filter codes
+pub const ARCHIVE_FILTER_NONE: c_int = 0;
+pub const ARCHIVE_FILTER_GZIP: c_int = 1;
+pub const ARCHIVE_FILTER_BZIP2: c_int = 2;
+pub const ARCHIVE_FILTER_COMPRESS: c_int = 3;
+pub const ARCHIVE_FILTER_PROGRAM: c_int = 4;
+pub const ARCHIVE_FILTER_LZMA: c_int = 5;
+pub const ARCHIVE_FILTER_XZ: c_int = 6;
+pub const ARCHIVE_FILTER_UU: c_int = 7;
+pub const ARCHIVE_FILTER_RPM: c_int = 8;
+pub const ARCHIVE_FILTER_LZIP: c_int = 9;
+pub const ARCHIVE_FILTER_LRZIP: c_int = 10;
+pub const ARCHIVE_FILTER_LZOP: c_int = 11;
+pub const ARCHIVE_FILTER_GRZIP: c_int = 12;
+pub const ARCHIVE_FILTER_LZ4: c_int = 13;
+pub const ARCHIVE_FILTER_ZSTD: c_int = 14;
+
 
 pub enum ArchiveStruct {}
 pub enum ArchiveEntryStruct {}
@@ -22,6 +39,7 @@ pub type ArchiveCloseCallBack = unsafe extern "C" fn (archive: ArchiveStruct,
 extern "C" {
     pub fn archive_version_number() -> c_int;
     pub fn archive_version_string() -> *const c_char;
+    pub fn archive_errno(archive: *mut ArchiveStruct) -> c_int;
     pub fn archive_read_new() -> *mut ArchiveStruct;
     pub fn archive_read_support_filter_all(archive: *mut ArchiveStruct) -> c_int;
     pub fn archive_read_support_format_all(archive: *mut ArchiveStruct) -> c_int;
@@ -40,6 +58,7 @@ extern "C" {
 // #[cfg(test)]
 // mod tests {
 //     use super::*;
+
 
 //     #[test]
 //     fn it_works() {
